3f7d9338af4739836c802c3f7d17ac4dd4f29801
anosov1818 <anosov1818@gmail.com>
2018-06-12 03:55:22 +0200

    route fix

--- application/core/route.php
+++ application/core/route.php
@@ -5,11 +5,9 @@
 > цепляет классы контроллеров и моделей;
 > создает экземпляры контролеров страниц и вызывает действия этих контроллеров.
 */
-class Route
-{
+class Route {
 
-	static function start()
-	{
+	static function start()	{
 		// контроллер и действие по умолчанию
 		$controller_name = 'Main';
 		$action_name = 'index';
@@ -17,15 +15,13 @@ class Route
 		$routes = explode('/', $_SERVER['REQUEST_URI']);
 
 		// получаем имя контроллера
-		if ( !empty($routes[1]) )
-		{	
-			$controller_name = $routes[1];
+		if ( !empty($routes[2]) ) {
+			$controller_name = $routes[2];
 		}
 		
 		// получаем имя экшена
-		if ( !empty($routes[2]) )
-		{
-			$action_name = $routes[2];
+		if ( !empty($routes[3]) ) {
+			$action_name = $routes[3];
 		}
 
 		// добавляем префиксы
@@ -43,20 +39,16 @@ class Route
 
 		$model_file = strtolower($model_name).'.php';
 		$model_path = "application/models/".$model_file;
-		if(file_exists($model_path))
-		{
+		if(file_exists($model_path)) {
 			include "application/models/".$model_file;
 		}
 
 		// подцепляем файл с классом контроллера
 		$controller_file = strtolower($controller_name).'.php';
 		$controller_path = "application/controllers/".$controller_file;
-		if(file_exists($controller_path))
-		{
+		if(file_exists($controller_path)) {
 			include "application/controllers/".$controller_file;
-		}
-		else
-		{
+		} else {
 			/*
 			правильно было бы кинуть здесь исключение,
 			но для упрощения сразу сделаем редирект на страницу 404
@@ -68,25 +60,19 @@ class Route
 		$controller = new $controller_name;
 		$action = $action_name;
 		
-		if(method_exists($controller, $action))
-		{
+		if(method_exists($controller, $action)) {
 			// вызываем действие контроллера
 			$controller->$action();
-		}
-		else
-		{
+		} else {
 			// здесь также разумнее было бы кинуть исключение
 			Route::ErrorPage404();
 		}
-	
 	}
 
-	function ErrorPage404()
-	{
+	static function ErrorPage404() {
         $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
         header('HTTP/1.1 404 Not Found');
 		header("Status: 404 Not Found");
 		header('Location:'.$host.'404');
     }
-    
 }
