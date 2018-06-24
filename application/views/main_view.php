c90f07ce9b77bbf33dbb0b32bdc52582fbd5176b
DanApro <dan.apro@hotmail.com>
2018-06-24 02:33:28 +0200

    css changes

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,4 +1,37 @@
+<div id="welcome">
 <h1>Welcome to Lovanda</h1>
 <p>Lovanda is a revolutionary application to find a partner online for free</p>
-<button>Sign Up</button>
+<button><a href="<?php print HTTP; ?>services">Sign Up</a></button>
+</div>
 
+<div class="holder">
+<div class="options">
+    <h3>Manage Invoices Online</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Eaque inventore saepe et eligendi reiciendis minima cumque 
+        necessitatibus sapiente mollitia accusantium ipsum, nulla 
+        repudiandae dolore voluptatibus cupiditate perspiciatis quae
+         debitis nemo.
+    </p>
+</div>
+
+<div class="options">
+    <h3>Dashboards and Reports</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Eaque inventore saepe et eligendi reiciendis minima cumque 
+        necessitatibus sapiente mollitia accusantium ipsum, nulla 
+        repudiandae dolore voluptatibus cupiditate perspiciatis quae
+         debitis nemo.
+    </p>
+</div>
+
+<div class="options">
+    <h3>Invite Share and Collaborate</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Eaque inventore saepe et eligendi reiciendis minima cumque 
+        necessitatibus sapiente mollitia accusantium ipsum, nulla 
+        repudiandae dolore voluptatibus cupiditate perspiciatis quae
+         debitis nemo.
+    </p>
+</div>
+</div>
