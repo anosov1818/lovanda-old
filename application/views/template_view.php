b76d558775d7289c81dfe201f0fffebee9a6dba6
anosov1818 <anosov1818@gmail.com>
2018-06-25 00:30:53 +0200

    fix html and css bugs

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
-Released for free under a Creative Commons Attribution 3.0 License
-
-Name       : Accumen
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20120712
-
-Modified by VitalySwipe
--->
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<title>Lovanda | Dating Service</title>
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
-		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="<?php print HTTP; ?>css/style.css" />
         <link href="https://fonts.googleapis.com/css?family=Comfortaa|Oxygen|Raleway|Sarala|Work+Sans" rel="stylesheet">
-        <script src="<?php print HTTP; ?>js/jquery-1.6.2.js" type="text/javascript"></script
+        <script src="<?php print HTTP; ?>js/jquery-1.6.2.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<div id="menu">
 			<ul>
-				<li #id="logo" class="first active"><a href="<?php print HTTP; ?>">Lovanda</a></li>
+				<li id="logo" class="first active"><a href="<?php print HTTP; ?>">Lovanda</a></li>
 				<li class="first active"><a href="<?php print HTTP; ?>">Home</a></li>
 				<li><a href="<?php print HTTP; ?>services">Dating</a></li>
 				<li><a href="<?php print HTTP; ?>portfolio">Messages</a></li>
@@ -54,7 +40,7 @@ Modified by VitalySwipe
 				<br class="clearfix" />
 			</div>
 		<div id="footer">
-			<a href="<?php print HTTP; ?>">Lovanda</a> &copy; 2018</a>
+			<a href="<?php print HTTP; ?>">Lovanda</a> &copy; 2018
 		</div>
 	</body>
 </html>
