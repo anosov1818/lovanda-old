35bcbaf4ab530390c7c0488d20f63a049c76bc63
DanApro <dan.apro@hotmail.com>
2018-06-28 09:11:36 +0200

    name of change/commit

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,13 +1,13 @@
 <div id="welcome">
 <h1>Welcome to Lovanda</h1>
 <p>Lovanda is a revolutionary application to find a partner online for free</p>
-<button><a href="<?php print HTTP; ?>services">Sign Up</a></button>
+<button class="signup"><a href="<?php print HTTP; ?>services">Sign Up</a></button>
 </div>
 
 <div class="holder">
 <div class="options">
     <h3>Manage Invoices Online</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    <p id="firstp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Eaque inventore saepe et eligendi reiciendis minima cumque 
         necessitatibus sapiente mollitia accusantium ipsum, nulla 
         repudiandae dolore voluptatibus cupiditate perspiciatis quae
@@ -17,7 +17,7 @@
 
 <div class="options">
     <h3>Dashboards and Reports</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    <p id="secondp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Eaque inventore saepe et eligendi reiciendis minima cumque 
         necessitatibus sapiente mollitia accusantium ipsum, nulla 
         repudiandae dolore voluptatibus cupiditate perspiciatis quae
@@ -27,7 +27,7 @@
 
 <div class="options">
     <h3>Invite Share and Collaborate</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    <p id="thirdp">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Eaque inventore saepe et eligendi reiciendis minima cumque 
         necessitatibus sapiente mollitia accusantium ipsum, nulla 
         repudiandae dolore voluptatibus cupiditate perspiciatis quae
