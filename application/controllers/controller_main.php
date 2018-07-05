6f73b0a64e6c770a8c4ecbfd65deda36f61f063e
anosov1818 <anosov1818@gmail.com>
2018-07-05 22:36:09 +0200

    main page form handler

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -3,6 +3,24 @@
 class Controller_Main extends Controller {
 
 	function action_index() {
+	    $name = '';
+        $username = '';
+        $email = '';
+        $password = '';
+        $repeat_password = '';
+
+        if($password !== $repeat_password){
+            print "passwords are different";
+            return false;
+        }
+
+        $user = new User();
+        $user->username = $username;
+        $user->password = sha1($password);
+        $user->name = $name;
+        $user->email = $email;
+
+
 		$this->view->generate('main_view.php', 'template_view.php');
 	}
 }
