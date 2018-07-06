5443b31e25d245561d1d0ba6ea84b8e39119958d
DanApro <dan.apro@hotmail.com>
2018-07-06 18:21:42 +0200

    bootstrap integration

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,38 +1,38 @@
 <div id="welcome">
     <h1>Welcome to Lovanda</h1>
     <p>Lovanda is a revolutionary application to find a partner online for free</p>
-    <button id="signup-form-button"> Sign up </button>
+    <button class="signup-button" id="signup-form-button"> Sign up </button>
 </div>
 
 <div id="form">
- <form action="" class="main-form needs-validation" novalidate>
+    <form action="" class="main-form needs-validation" novalidate>
         <div class="row">
             <div class="col">
-        <div class="form-group">
-            <label for="gender">I Am</label>
-            <select name="gender" class="form-control" placeholder="">
-                <option value="male">Man</option>
-                <option value="female">Woman</option>
-                <option value="wtf">Other</option>
-            </select>
-        </div>
+                <div class="form-group">
+                    <label for="gender">I Am</label>
+                    <select name="gender" class="form-control" placeholder="">
+                        <option value="male">Man</option>
+                        <option value="female">Woman</option>
+                        <option value="wtf">Other</option>
+                    </select>
+                </div>
             </div>
             <div class="col">
-            <div class="form-group">
-                <label for="gender">Looking For</label>
-                <select name="gender" class="form-control"placeholder="">
-                    <option value="male">Man</option>
-                    <option value="female">Woman</option>
-                    <option value="wtf">Other</option>
-                </select>
-              </div>
+                <div class="form-group">
+                    <label for="gender">Looking For</label>
+                    <select name="gender" class="form-control"placeholder="">
+                        <option value="male">Man</option>
+                        <option value="female">Woman</option>
+                        <option value="wtf">Other</option>
+                    </select>
+                </div>
             </div>
         </div>
         <div class="form-group">
-        <label for="Name">Your Name</label>
+            <label for="Name">Your Name</label>
             <input type="text" name="lastname" id="lastname" class="form-control">
             <div class="invalid-feedback">Please enter your email.</div> 
-                 </div>       
+        </div>       
         <div class="form-group">
             <label for="username">Email</label>
             <input type="email" name="email" id="email" class="form-control" required>
@@ -43,19 +43,20 @@
             <input type="password" name="password" id="password" class="form-control">
         </div>
         <div id="terms" class="form-group">
-        <div class="form-check">
-            <input type="checkbox" id="accept-terms" class="form-check-input">
-            <label for="accept-terms" class="form-check-label">
-                By registering,              
-                I confirm that I am 18 years old or older.             
-                I accept the terms of the              
-                <a href="#">user agreement</a> and             
-                <a href="#">privacy policy</a>.</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="form-check">
+                <input type="checkbox" id="accept-terms" class="form-check-input">
+                <label for="accept-terms" class="form-check-label">
+                    By registering,              
+                    I confirm that I am 18 years old or older.             
+                    I accept the terms of the              
+                    <a href="#">user agreement</a> and             
+                    <a href="#">privacy policy</a>.
+                </label>
+            </div>
+            <button class="signup-button">Submit</button>
         </div>
     </form>
-    </div>
+</div>
 
 <div class="options">
     <h3>Manage Invoices Online</h3>
