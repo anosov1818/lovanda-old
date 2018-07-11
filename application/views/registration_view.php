04ec818c1e503da9b7a6532f4d58cbbd910d8812
DanApro <dan.apro@hotmail.com>
2018-07-12 00:47:43 +0200

    beautiful form

--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -1,5 +1,238 @@
 <h1>Registration</h1>
 
+<div id="reg-div" class="container">
+
+
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Photo</label>
+    <input type="file" class="form-control-file my-col col-lg-9" id="exampleFormControlFile1">
+</div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Country</label>
+            <select class="form-control my-col col-lg-9">
+              <option>Default select</option>
+            </select>
+</div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">City</label>
+            <select class="form-control my-col col-lg-9">
+              <option>Default select</option>
+            </select>
+</div>
+<!-- should we make it until 90 with js? -->
+<div class="form-group row my-row">
+  <label class="my-col col-lg-3" for="exampleFormControlSelect1">Age</label>
+  <select class="form-control my-col col-lg-9" id="exampleFormControlSelect1">
+    <option>18</option>
+  </select>
+</div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Education</label>
+            <select class="form-control my-col col-lg-9">
+              <option>secondary education</option>
+              <option>incomplete higher education</option>
+              <option>bachelor's degree</option>
+              <option>master's degree</option>
+              <option>PhD</option>
+            </select>
+  </div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="exampleFormControlTextarea1">Hobbies</label>
+    <textarea class="form-control my-col col-lg-9" id="exampleFormControlTextarea1" rows="4"></textarea>
+</div>
+
+<div class="form-group row my-row">
+  <label class="my-col col-lg-3" for="">Languages</label>
+  <select class="form-control my-col col-lg-9">
+<option>English</option>
+<option>Abkhaz</option>
+<option>Adyghe</option>
+<option>Afrikaans</option>
+<option>Akan</option>
+<option>Albanian</option>
+<option>American Sign Language</option>
+<option>Amharic</option>
+<option>Arabic</option>
+<option>Aragonese</option>
+<option>Aramaic</option>
+<option>Armenian</option>
+<option>Aymara</option>
+<option>Balinese</option>
+<option>Basque</option>
+<option>Betawi</option>
+<option>Bosnian</option>
+<option>Breton</option>
+<option>Bulgarian</option>
+<option>Cantonese</option>
+<option>Catalan</option>
+<option>Cherokee</option>
+<option>Chickasaw</option>
+<option>Chinese</option>
+<option>Coptic</option>
+<option>Cornish</option>
+<option>Corsican</option>
+<option>Crimean Tatar</option>
+<option>Croatian</option>
+<option>Czech</option>
+<option>Danish</option>
+<option>Dutch</option>
+<option>Esperanto</option>
+<option>Estonian</option>
+<option>Ewe</option>
+<option>Fiji Hindi</option>
+<option>Filipino</option>
+<option>Finnish</option>
+<option>French</option>
+<option>Galician</option>
+<option>Georgian</option>
+<option>German</option>
+<option>Greek, Modern</option>
+<option>Ancient Greek</option>
+<option>Greenlandic</option>
+<option>Haitian Creole</option>
+<option>Hawaiian</option>
+<option>Hindi</option>
+<option>Hungarian</option>
+<option>Icelandic</option>
+<option>Indonesian</option>
+<option>Inuktitut</option>
+<option>Interlingua</option>
+<option>Irish</option>
+<option>Italian</option>
+<option>Japanese</option>
+<option>Javanese</option>
+<option>Kabardian</option>
+<option>Kalasha</option>
+<option>Kannada</option>
+<option>Kashubian</option>
+<option>Khmer</option>
+<option>Kinyarwanda</option>
+<option>Korean</option>
+<option>Kurdish/Kurdî</option>
+<option>Ladin</option>
+<option>Latgalian</option>
+<option>Latin</option>
+<option>Lingala</option>
+<option>Livonian</option>
+<option>Lojban</option>
+<option>Lower Sorbian</option>
+<option>Low German</option>
+<option>Macedonian</option>
+<option>Malay</option>
+<option>Malayalam</option>
+<option>Mandarin</option>
+<option>Manx</option>
+<option>Maori</option>
+<option>Mauritian Creole</option>
+<option>Middle English</option>
+<option>Middle Low German</option>
+<option>Mongolian</option>
+<option>Norwegian</option>
+<option>Old Armenian</option>
+<option>Old English</option>
+<option>Old French</option>
+<option>Old Javanese</option>
+<option>Old Norse</option>
+<option>Old Prussian</option>
+<option>Oriya</option>
+<option>Pangasinan</option>
+<option>Papiamentu</option>
+<option>Pashto</option>
+<option>Persian</option>
+<option>Pitjantjatjara</option>
+<option>Polish</option>
+<option>Portuguese</option>
+<option>Proto-Slavic</option>
+<option>Quenya</option>
+<option>Rajasthani</option>
+<option>Rapa Nui</option>
+<option>Romanian</option>
+<option>Russian</option>
+<option>Sanskrit</option>
+<option>Scots</option>
+<option>Scottish Gaelic</option>
+<option>Serbian</option>
+<option>Serbo-Croatian</option>
+<option>Slovak</option>
+<option>Slovene</option>
+<option>Spanish</option>
+<option>Sinhalese</option>
+<option>Swahili</option>
+<option>Swedish</option>
+<option>Tagalog</option>
+<option>Tajik</option>
+<option>Tamil</option>
+<option>Tarantino</option>
+<option>Telugu</option>
+<option>Thai</option>
+<option>Tok Pisin</option>
+<option>Turkish</option>
+<option>Twi</option>
+<option>Ukrainian</option>
+<option>Upper Sorbian</option>
+<option>Urdu</option>
+<option>Uyghur</option>
+<option>Uzbek</option>
+<option>Venetian</option>
+<option>Vietnamese</option>
+<option>Vilamovian</option>
+<option>Volapük</option>
+<option>Võro</option>
+<option>Welsh</option>
+<option>Xhosa</option>
+<option>Yiddish</option>
+<option>Zazaki</option>
+<option>other</option>
+  </select>
+
+</div>
+
+
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Marital Status</label>
+            <select class="form-control my-col col-lg-9">
+              <option>not married</option>
+              <option>divorced</option>
+              <option>widower/widow</option>
+              <option>other</option>
+            </select>
+</div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Smoking</label>
+    <select class="form-control my-col col-lg-9">
+      <option>non-smoker</option>
+      <option>sometimes</option>
+      <option>several times a week</option>
+      <option>everyday</option>
+      <option>I've quit smoking</option>
+    </select>
+</div>
+
+<div class="form-group row my-row">
+    <label class="my-col col-lg-3" for="">Alcohol</label>
+    <select class="form-control my-col col-lg-9">
+      <option>never</option>
+      <option>once or twice a year</option>
+      <option>once or twice a month</option>
+      <option>once a week</option>
+      <option>several times a week</option>
+      <option>everyday</option>
+      <option>I only drink beer</option>
+      <option>I'm trying to quit</option>
+      <option>I've quit drinking</option>
+    </select>
+</div>
+
+</div>
+
+
 <?php
 
     foreach($data as $row) {
@@ -8,4 +241,3 @@
     }
 
 ?>
-
