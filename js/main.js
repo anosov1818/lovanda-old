8c977fea558cc7cc72385856e94147bb67205196
anosov1818 <anosov1818@gmail.com>
2018-10-28 01:50:32 +0200

    select outline none

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ $( "#signup-form-button" ).on("click", function() {
     $( "#form").fadeIn();
 });
 
+//
+
 //singup form validation
 var form = document.querySelector(".needs-validation");
 
@@ -20,8 +22,7 @@ form.addEventListener("submit", function(event) {
 
 //confirm password
 var check = function() {
-  if (document.getElementById('password').value ==
-    document.getElementById('confirm-password').value) {
+  if (document.getElementById('password').value == document.getElementById('confirm-password').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
   } else {
@@ -31,7 +32,7 @@ var check = function() {
 };
 
 //slider, dude I know this is a disaster, I need more time for this
-var profiles =[];
+/*var profiles =[];
 
 var people = document.querySelectorAll(".profile1, .profile2, .profile3")
 var btn = document.querySelectorAll("button");
@@ -40,3 +41,4 @@ var counter = 1;
 btn.addEventListener("click", function(){
   
 });
+*/
